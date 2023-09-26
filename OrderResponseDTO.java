96ffa92b80d1be5fb62ba5c9e9c3b0f33829879b
laxmangandi369 <119059799+laxmangandi369@users.noreply.github.com>
2023-09-26 08:21:13 +0530

    Create OrderResponseDTO.java

---
+++ OrderResponseDTO.java
@@ -0,0 +1,32 @@
+package com.javatechie.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class OrderResponseDTO {
+
+    System.out.println("github testing");
+    System.out.println("github testing");
+System.out.println("github testing");
+System.out.println("github testing");
+System.out.println("github testing");
+System.out.println("github testing");
+System.out.println("github testing");
+System.out.println("github testing");
+System.out.println("github testing");
+System.out.println("github testing");
+    private String orderId;
+    private String name;
+    private int qty;
+    private double price;
+    private Date orderDate;
+    private String status;
+    private int estimateDeliveryWindow;
+}
